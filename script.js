b63ec2492ae6141cee94ca7c59164d33f68b7d2b
Diego Martins de Pinho <diegomartinsdepinho@msn.com>
2025-01-27 18:35:00 -0300

    refact js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 document.addEventListener('DOMContentLoaded', () => {
+  const QTD_POSTS = 1;
   const container = document.querySelector(".container");
-  const cardTemplate = document.getElementById("card-template");
-  for (let i = 0; i < 1; i++) {
+  const cardTemplate = document.querySelector("#card-template");
+  for (let i = 0; i < QTD_POSTS; i++) {
     container.append(cardTemplate.content.cloneNode(true));
   }
 
@@ -10,12 +11,14 @@ document.addEventListener('DOMContentLoaded', () => {
     .then((posts) => {
       container.innerHTML = "";
       posts.forEach((post) => {
+        const { title, details, coverImage, logoImage, link } = post;
+
         const div = cardTemplate.content.cloneNode(true);
-        div.getElementById("card-link").href = post.link;
-        div.getElementById("logo-img").src = post.logoImage;
-        div.getElementById("card-title").textContent = post.title;
-        div.getElementById("card-details").textContent = post.details;
-        div.getElementById("cover-img").src = post.coverImage;
+        div.querySelector("#card-link").href = link;
+        div.querySelector("#logo-img").src = logoImage;
+        div.querySelector("#card-title").textContent = title;
+        div.querySelector("#card-details").textContent = details;
+        div.querySelector("#cover-img").src = coverImage;
         container.append(div);
       });
     });
